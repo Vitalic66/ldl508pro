d11cccd5cb5393bd73f078fc94d8dc88cdd35a8c
Vitalic66 <red-dot1@web.de>
2026-07-31 11:08:19 +0200

    Delete ldl508pro directory

--- ldl508pro/sensor.py
+++
@@ -1,5 +0,0 @@
-"""Phase 2 sensor entities are configured directly inside the ldl508pro block.
-
-A separate sensor platform may be introduced later if it improves the public
-configuration API.
-"""
--- ldl508pro/text_sensor.py
+++
@@ -1,6 +0,0 @@
-"""Reserved for a later project phase.
-
-Phase 1 only registers the central LDL508PRO UART component. Native ESPHome
-entities will be added incrementally after the UART and configuration layers
-are working and tested.
-"""
